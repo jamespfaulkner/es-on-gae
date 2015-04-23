6852e2f001fb01a7590ecdb6ec521f49aa160980
James Faulkner <jamespfaulkner@gmail.com>
2015-04-23 12:29:23 +0100

    chore (cleaning up)

--- src/main/java/com/foodit/example/es/SearcherService.java
+++ src/main/java/com/foodit/example/es/SearcherService.java
@@ -1,1 +1,1 @@
-package com.foodit.example.es;public interface SearcherService {    public String searchByQueryString(String query, int from, int resultsSize);}
+package com.foodit.example.es;public interface SearcherService {    String searchByQueryString(String query, int from, int resultsSize);}
